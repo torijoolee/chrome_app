b501d5274f19824e1a6e7d0cbe30c32ebfc51c5e
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-01-05 15:41:08 +0900

    HTML,JS: Saved username

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(() => {
+  const userForm = document.querySelector("#user__form");
+  const userInput = document.querySelector(".user__input");
+  const userGreeting = document.querySelector("#greeting");
+
+  function userSubmitHandle(event) {
+    event.preventDefault();
+    userForm.classList.add("hidden");
+    const usernameInput = userInput.value;
+    localStorage.setItem("username", userInput.value);
+    paintGreeting(usernameInput);
+  }
+  userForm.addEventListener("submit", userSubmitHandle);
+
+  // 사용자 이름 화면에 남기기
+  function paintGreeting(usernameInput) {
+    userGreeting.innerText = `hello ${usernameInput}`;
+    userGreeting.classList.remove("hidden");
+  }
+  const localUsername = localStorage.getItem("username");
+  if (localUsername === null) {
+    userForm.classList.remove("hidden");
+    userForm.addEventListener("submit", userSubmitHandle);
+  } else {
+    //username있다면 폼에 히든클래스 추가 폼을 숨긴다.
+    //h1에 이름이 보이도록 한다.
+    paintGreeting(localUsername);
+  }
+})();
