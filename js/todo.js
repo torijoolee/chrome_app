eb7d76b04a88fb8f61c2efa738852ae93c1151d6
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-01-08 17:50:52 +0900

    JS: Stringify and parse todos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const todoForm = document.querySelector("#todo__form");
 const todoInput = document.querySelector(".todo__input");
 const todoList = document.querySelector(".todo__list");
 
+const toDos = [];
+
+// toDos in localstorage
+function todosInLocal() {
+  localStorage.setItem("todoList", JSON.stringify(toDos));
+}
+
 //delete target todo item
 function deleteTodoItem(event) {
   const targetTodoItem = event.target.parentNode;
@@ -18,7 +25,6 @@ function paintTodo(todoText) {
   liTextItems.appendChild(spanTextItem);
   liTextItems.appendChild(btnTextItem);
   liTextItems.addEventListener("click", deleteTodoItem);
-
   todoList.appendChild(liTextItems);
 }
 
@@ -26,7 +32,16 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const todoText = todoInput.value;
   todoInput.value = "";
+  toDos.push(todoText);
   paintTodo(todoText);
+  todosInLocal();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const tosaveToDos = localStorage.getItem("todoList");
+if (tosaveToDos) {
+  const parseTodos = JSON.parse(tosaveToDos);
+  console.log(parseTodos);
+  parseTodos.forEach((item) => console.log(`hello ${item}`));
+}
